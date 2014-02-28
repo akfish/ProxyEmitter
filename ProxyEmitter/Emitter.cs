b6926435dd34143048e060b7ff920e0b295fd9c7
AKFish <akfish@gmail.com>
2014-02-28 22:19:45 +0800

    Implement Emit

---
+++ ProxyEmitter/Emitter.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Threading;
+
+namespace ProxyEmitter
+{
+    /// <summary>
+    /// Helper class
+    /// </summary>
+    public static class Emitter
+    {
+        /// <summary>
+        /// Gets the <see cref="AppDomain"/> of current thread
+        /// </summary>
+        public static AppDomain CurrentDomain
+        {
+            get { return Thread.GetDomain(); }
+        }
+
+        # region Global Methods
+
+        /// <summary>
+        /// Creates an <see cref="AssemblyBuilder"/> instance within <see cref="CurrentDomain"/> 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="access"></param>
+        /// <returns></returns>
+        public static AssemblyBuilder GetAssemblyBuilder(string name, AssemblyBuilderAccess access = AssemblyBuilderAccess.RunAndSave)
+        {
+            var aname = new AssemblyName(name);
+            AppDomain currentDomain = AppDomain.CurrentDomain; 
+            AssemblyBuilder builder = currentDomain.DefineDynamicAssembly(aname, access);
+            return builder;
+        }
+
+        /// <summary>
+        /// Creates a <see cref="ModuleBuilder"/> instance within <paramref name="asmBuilder"/>
+        /// </summary>
+        /// <param name="asmBuilder"></param>
+        /// <param name="moduleName"></param>
+        /// <returns></returns>
+        public static ModuleBuilder GetModule(AssemblyBuilder asmBuilder, String moduleName)
+        {
+            ModuleBuilder builder = asmBuilder.DefineDynamicModule(moduleName);//"EmitMethods", "EmitMethods.dll");
+            return builder;
+        }
+
+        /// <summary>
+        /// Creates an <see cref="EnumBuilder"/> instance within <paramref name="modBuilder"/>
+        /// </summary>
+        /// <param name="modBuilder"></param>
+        /// <param name="enumName"></param>
+        /// <returns></returns>
+        public static EnumBuilder GetEnum(ModuleBuilder modBuilder, string enumName)
+        {
+            EnumBuilder builder = modBuilder.DefineEnum(enumName, TypeAttributes.Public, typeof(Int32));
+            return builder;
+        }
+
+        /// <summary>
+        /// Create a <see cref="TypeBuilder"/> instance within <paramref name="modBuilder"/>
+        /// </summary>
+        /// <param name="modBuilder"></param>
+        /// <param name="className"></param>
+        /// <param name="parent"></param>
+        /// <param name="interfaces"></param>
+        /// <returns></returns>
+        public static TypeBuilder GetType(ModuleBuilder modBuilder, string className, Type parent = null, Type[] interfaces = null)
+        {
+            if (parent == null)
+                parent = typeof(Object);
+            TypeBuilder builder = modBuilder.DefineType(className, TypeAttributes.Public, parent, interfaces);
+            return builder;
+        }
+
+        public static TypeBuilder GetType(ModuleBuilder modBuilder, string className, params string[] genericparameters)
+        {
+            TypeBuilder builder = modBuilder.DefineType(className, TypeAttributes.Public);
+            GenericTypeParameterBuilder[] genBuilders = builder.DefineGenericParameters(genericparameters);
+
+            foreach (GenericTypeParameterBuilder genBuilder in genBuilders) // We take each generic type T : class, new()
+            {
+                genBuilder.SetGenericParameterAttributes(GenericParameterAttributes.ReferenceTypeConstraint | GenericParameterAttributes.DefaultConstructorConstraint);
+                //genBuilder.SetInterfaceConstraints(interfaces);
+            }
+
+            return builder;
+        }
+
+        public static MethodBuilder GetMethod(TypeBuilder typBuilder, string methodName, MethodAttributes attributes)
+        {
+            MethodBuilder builder = typBuilder.DefineMethod(
+                methodName,
+                attributes);
+            return builder;
+        }
+        public static MethodBuilder GetMethod(TypeBuilder typBuilder, string methodName, MethodAttributes attributes, Type returnType, params Type[] parameterTypes)
+        {
+            MethodBuilder builder = typBuilder.DefineMethod(
+                methodName,
+                attributes,
+                CallingConventions.HasThis,
+                returnType,
+                parameterTypes);
+            return builder;
+        }
+
+        public static MethodBuilder GetMethod(TypeBuilder typBuilder, string methodName, MethodAttributes attributes, Type returnType, string[] genericParameters, params Type[] parameterTypes)
+        {
+            MethodBuilder builder = typBuilder.DefineMethod(
+                methodName,
+                attributes,
+                CallingConventions.HasThis,
+                returnType, parameterTypes);
+
+            GenericTypeParameterBuilder[] genBuilders = builder.DefineGenericParameters(genericParameters);
+
+            foreach (GenericTypeParameterBuilder genBuilder in genBuilders) // We take each generic type T : class, new()
+            {
+                genBuilder.SetGenericParameterAttributes(GenericParameterAttributes.ReferenceTypeConstraint | GenericParameterAttributes.DefaultConstructorConstraint);
+                //genBuilder.SetInterfaceConstraints(interfaces);
+            }
+            return builder;
+        }
+        # endregion
+    }
+}
--- ProxyEmitter/ProxyEmitter.cs
+++ ProxyEmitter/ProxyEmitter.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
 
 namespace ProxyEmitter
 {
@@ -129,7 +131,7 @@ namespace ProxyEmitter
             where TBase : ProxyBase
             where TInterface : class
         {
-            return (TInterface) CreateProxy(typeof(TBase), typeof(TInterface));
+            return (TInterface)CreateProxy(typeof(TBase), typeof(TInterface));
         }
 
         #endregion
@@ -138,7 +140,163 @@ namespace ProxyEmitter
 
         private static Type EmitProxyType(Type baseType, Type interfaceType)
         {
-            throw new NotImplementedException();
+            AssemblyBuilder asmBuilder = Emitter.GetAssemblyBuilder("DynamicProxyAssembly");
+            ModuleBuilder mBuilder = Emitter.GetModule(asmBuilder, "EmittedProxies");
+            TypeBuilder tBuilder = Emitter.GetType(mBuilder, String.Format("{0}{1}Proxy", baseType.FullName, interfaceType), baseType, new[] { interfaceType });
+
+            // TODO: emit parametrized ctor 
+            var invokeMethod = baseType.GetMethod("Invoke");
+            var convertMethod = baseType.GetMethod("ConvertReturnValue");
+            // Create methods in interfaceType
+            foreach (var method in interfaceType.GetMethods())
+            {
+                EmitInterfaceMethod(tBuilder, method, invokeMethod, convertMethod);
+            }
+
+            return tBuilder.CreateType();
+        }
+
+        private static void EmitInterfaceMethod(TypeBuilder tBuilder, MethodInfo method, MethodInfo invokeMethod, MethodInfo convertMethod)
+        {
+            #region Emit Signatue
+            //  .method public hidebysig virtual instance void 
+            //      MethodName(xxx) cil managed
+            //  {
+            var pTypes = method.GetParameters().Select(p => p.ParameterType).ToArray();
+            MethodBuilder builder = Emitter.GetMethod(
+                tBuilder,
+                method.Name,
+                MethodAttributes.Public |
+                MethodAttributes.HideBySig |
+                MethodAttributes.Virtual,
+                method.ReturnType,
+                pTypes);
+            #endregion
+
+            var ilGen = builder.GetILGenerator();
+
+
+            EmitLocals(ilGen, method, pTypes);
+
+            EmitHead(ilGen, method);
+
+            EmitInvokeArguments(ilGen, method, pTypes);
+
+            EmitInvoke(ilGen, method, invokeMethod);
+
+            EmitReturn(ilGen, method, convertMethod);
+        }
+
+        private static void EmitLocals(ILGenerator ilGen, MethodInfo method, Type[] pTypes)
+        {
+            // Has return value
+            if (method.ReturnType != typeof(void))
+            {
+                ilGen.DeclareLocal(method.ReturnType);
+            }
+            // Has parameters
+            if (pTypes.Length > 0)
+                ilGen.DeclareLocal(typeof(object[]));
+
+        }
+
+        private static void EmitHead(ILGenerator ilGen, MethodInfo method)
+        {
+            // IL_0000: nop
+            ilGen.Emit(OpCodes.Nop);
+            // IL_0001: ldarg.0
+            ilGen.Emit(OpCodes.Ldarg_0);
+            // Twice if has return value
+            // IL_0002: ldarg.0
+            if (method.ReturnType != typeof(void))
+                ilGen.Emit(OpCodes.Ldarg_0);
+        }
+
+        private static void EmitInvokeArguments(ILGenerator ilGen, MethodInfo method, Type[] pTypes)
+        {
+            // First one, method name
+            // IL_0002: ldstr "Fn2"
+            ilGen.Emit(OpCodes.Ldstr, method.Name);
+
+            // Has arguments?
+            if (pTypes.Length == 0)
+            {
+                // No? Easy
+                // IL_0007: ldnull
+                ilGen.Emit(OpCodes.Ldnull);
+            }
+            else
+            {
+                // Yes? So much work
+                // Initialize array
+                // IL_0006:  ldc.i4.x
+                ilGen.Emit(OpCodes.Ldc_I4_S, pTypes.Length);
+                // IL_0007:  newarr     [mscorlib]System.Object
+                ilGen.Emit(OpCodes.Newarr, typeof(Object));
+
+                // If has return value, array is local 1. Otherwise 0.
+                var hasReturn = method.ReturnType != typeof(void);
+                var set_op = hasReturn ? OpCodes.Stloc_1 : OpCodes.Stloc_0;
+                var get_op = hasReturn ? OpCodes.Ldloc_1 : OpCodes.Stloc_0;
+                // IL_000c:  stloc.1
+                ilGen.Emit(set_op);
+
+                // More work coming. 
+                // Now fill the array
+                for (int i = 0; i < pTypes.Length; i++)
+                {
+                    // Load the array first
+                    // IL_000X + 00: ldloc.1
+                    ilGen.Emit(get_op);
+
+                    // Push the index
+                    // IL_000X + 01: ldc_i4_x
+                    ilGen.Emit(OpCodes.Ldc_I4_S, i);
+                    // Load argument i + 1 (note that argument 0 is this pointer(?))
+                    // IL_000X + 02: ldarg_X
+                    ilGen.Emit(OpCodes.Ldarga_S, i + 1);
+                    // Box value type
+                    if (pTypes[i].IsValueType)
+                    {
+                        // IL_000X + 03: box pTypes[i]
+                        ilGen.Emit(OpCodes.Box, pTypes[i]);
+                    }
+                    // Set arrary element
+                    // IL_00X + ??: stelem.ref
+                    ilGen.Emit(OpCodes.Stelem_Ref);
+                }
+
+                // Load array
+                // IL_00XX: ldloc.1
+                ilGen.Emit(get_op);
+            }
+        }
+
+        private static void EmitInvoke(ILGenerator ilGen, MethodInfo method, MethodInfo invokeMethod)
+        {
+            // IL_0022: callvirt instance object ProxyEmitter.ProxyBase::Invoke(string, object[])
+            ilGen.Emit(OpCodes.Callvirt, invokeMethod);
+        }
+
+        private static void EmitReturn(ILGenerator ilGen, MethodInfo method, MethodInfo convertMethod)
+        {
+            var hasReturn = method.ReturnType != typeof(void);
+            if (hasReturn)
+            {
+                // IL_000e: callvirt instance !!0 ProxyEmitter.ProxyBase::ConvertReturnValue<int32>(object)
+                ilGen.EmitCall(OpCodes.Callvirt, convertMethod, new[] { method.ReturnType });
+                // IL_0013: stloc.0
+                ilGen.Emit(OpCodes.Stloc_0);
+                // IL_0016: ldloc.0
+                ilGen.Emit(OpCodes.Ldloc_0);
+            }
+            else
+            {
+                // IL_000d: pop
+                ilGen.Emit(OpCodes.Pop);
+            }
+            // IL_000e: ret   
+            ilGen.Emit(OpCodes.Ret);
         }
 
         #endregion
